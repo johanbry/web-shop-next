1cf10734e81310f5d7e19ccc7cd04730d404413c
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-03 14:00:22 +0100

    functions to get categories and convert flat list to tree

---
+++ lib/category.ts
@@ -0,0 +1,57 @@
+import { ICategory, ICategoryInTree } from "@/interfaces/interfaces";
+import Category from "@/models/Category";
+import { ObjectId } from "mongoose";
+import connectToDB from "@/utils/db";
+
+/**
+ * Get all categories from database.
+ *
+ * @returns categories
+ */
+
+export const getAllCategories = async () => {
+  let categories: ICategory[] = [];
+  try {
+    await connectToDB();
+    categories = await Category.find({}).exec();
+  } catch (error) {
+    console.log((error as Error).message);
+  }
+
+  return categories;
+};
+
+/**
+ * Convert flat category list with parent refs to tree.
+ *
+ * @param categories
+ * @param parentId
+ * @returns categoryList
+ */
+
+export const convertCategoriesToTree = (
+  categories: ICategory[],
+  parentId: ObjectId | null = null
+) => {
+  const categoryList: ICategoryInTree[] = [];
+  let category;
+
+  if (parentId == undefined) {
+    category = categories.filter((cat) => cat.parent_id == undefined);
+  } else {
+    category = categories.filter(
+      (cat) => cat.parent_id?.toString() == parentId.toString()
+    );
+  }
+
+  for (let cat of category) {
+    categoryList.push({
+      _id: cat._id,
+      name: cat.name,
+      slug: cat.slug,
+      children: convertCategoriesToTree(categories, cat._id),
+    });
+  }
+
+  return categoryList;
+};
