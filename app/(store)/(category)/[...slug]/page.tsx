2779890fdd01ee4db47b9bb23a8a9de4d5d7df72
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-09 19:55:44 +0100

    category page basics without product listing

---
+++ app/(store)/(category)/[...slug]/page.tsx
@@ -0,0 +1,79 @@
+import {
+  Anchor,
+  Box,
+  Breadcrumbs,
+  Title,
+  Text,
+  Button,
+  Container,
+} from "@mantine/core";
+import { Metadata } from "next";
+import { getCategoryBySlug, getSubCategories } from "@/lib/category";
+import Link from "next/link";
+import { Fragment } from "react";
+
+type Props = {
+  params: { slug: string[] };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const categorySlug = params.slug[params.slug.length - 1]; // The slug of the current category
+  const category = await getCategoryBySlug(categorySlug);
+
+  return {
+    title: category?.name,
+    description: category?.description,
+  };
+}
+
+export default async function CategoryPage({ params }: Props) {
+  const categorySlug = params.slug[params.slug.length - 1]; // The slug of the current category
+  const category = await getCategoryBySlug(categorySlug);
+  const currentPath = params.slug.join("/"); // The path of the current category
+  if (!category) return <div>Category not found</div>;
+
+  const subCategories = await getSubCategories(category._id);
+
+  const breadcrumbItems = await Promise.all(
+    params.slug.map(async (slug, index) => {
+      const href = `/${params.slug.slice(0, index + 1).join("/")}`;
+      const cat = await getCategoryBySlug(slug);
+      const name = cat?.name;
+      return (
+        <Fragment key={index}>
+          {index === 0 && (
+            <Anchor size="sm" href={"/"}>
+              Hem
+            </Anchor>
+          )}
+          <Anchor size="sm" href={href}>
+            {name}
+          </Anchor>
+        </Fragment>
+      );
+    })
+  );
+
+  const subCategoryItems = subCategories.map((cat) => (
+    <Button
+      size="xs"
+      radius={3}
+      component={Link}
+      variant="outline"
+      href={`/${currentPath}/${cat.slug}`}
+      key={cat._id.toString()}
+    >
+      {cat.name}
+    </Button>
+  ));
+
+  return (
+    <Container size="lg">
+      <Breadcrumbs mb={6}>{breadcrumbItems}</Breadcrumbs>
+      <Title mb={8}>{category?.name}</Title>
+      <Text>{category?.description}</Text>
+
+      <Box my={10}>{subCategoryItems}</Box>
+    </Container>
+  );
+}
