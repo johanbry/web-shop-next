f7d47d624e24fe510e88cc99fb20d40036e54e8c
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-09 19:53:51 +0100

    add description property

--- models/Category.ts
+++ models/Category.ts
@@ -5,6 +5,10 @@ const CategorySchema = new Schema({
     type: String,
     required: true,
   },
+  description: {
+    type: String,
+    required: true,
+  },
   slug: {
     type: String,
     unique: true,
