77eaef0ac831c3e140170b6346e5a0a447b57fc3
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-09 19:53:33 +0100

    add description property to category

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -3,13 +3,15 @@ import { ObjectId } from "mongoose";
 export interface ICategory {
   _id: ObjectId;
   name: string;
+  description?: string;
   slug: string;
-  parent_id: ObjectId | null;
+  parent_id?: ObjectId | null;
 }
 
 export interface ICategoryInTree {
   _id: string;
   name: string;
+  description?: string;
   slug: string;
   children?: (ICategoryInTree | null)[] | null;
 }
