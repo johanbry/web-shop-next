2402ddaab0e475652d1d7fc321573255be4089b6
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-11 11:32:53 +0100

    function count total products

--- lib/product.ts
+++ lib/product.ts
@@ -1,6 +1,6 @@
 import Product from "@/models/Product";
 import { ObjectId } from "mongodb";
-import { PipelineStage, Schema } from "mongoose";
+import { Document, PipelineStage, Schema } from "mongoose";
 
 /**
  * Get products from database, depending on category, search term, sort order, offset and limit.
@@ -23,7 +23,7 @@ export const getProducts = async (
   sortOrder?: string,
   searchTerm?: string
 ) => {
-  let products = [];
+  let products: Document[] = [];
   const pipeline: PipelineStage[] = [];
 
   if (categoryId) {
@@ -53,4 +53,36 @@ export const getProducts = async (
     console.log((error as Error).message);
   }
   return products;
+};
+
+/**
+ * Calculates the total number of products.
+ * If a categoryId is provided, it calculates the total number of products in that category.
+ * @param categoryId - The ID of the category (optional)
+ * @returns The total number of products
+ */
+export const totalProducts = async (categoryId?: string) => {
+  const pipeline: PipelineStage[] = [
+    {
+      $unwind: { path: "$style.options", preserveNullAndEmptyArrays: true },
+    },
+    { $count: "total" },
+  ];
+  let total = 0;
+
+  if (categoryId) {
+    pipeline.unshift({
+      $match: {
+        categories: {
+          $elemMatch: {
+            $eq: new ObjectId(categoryId),
+          },
+        },
+      },
+    });
+  }
+  const doc = await Product.aggregate(pipeline);
+  console.log("doc", doc);
+  if (doc.length > 0) total = doc[0].total;
+  return total;
 };
