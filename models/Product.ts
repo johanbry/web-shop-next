9c1986014fd053a467370773a38bac98d07032f1
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-14 21:30:42 +0100

    add and edit model properties

--- models/Product.ts
+++ models/Product.ts
@@ -38,7 +38,6 @@ const ProductSchema = new Schema(
     },
     images: {
       type: [String],
-      default: "no-image.jpg",
     },
     style: {
       type: {
@@ -49,7 +48,7 @@ const ProductSchema = new Schema(
       },
       options: [
         {
-          article_id: {
+          style_id: {
             type: String,
             required: [true, "Please add a article_id"],
           },
@@ -57,6 +56,10 @@ const ProductSchema = new Schema(
             type: String,
             required: [true, "Please add a name"],
             maxlength: [50, "Name can not be more than 50 characters"],
+          },
+          slug: String,
+          description: {
+            type: String,
           },
           product_name: {
             type: String,
