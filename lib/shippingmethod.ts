e83e40d745c5124035119fd43f6bba84fe451de3
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-23 20:40:23 +0100

    get shipping methods function

---
+++ lib/shippingmethod.ts
@@ -0,0 +1,16 @@
+import { IShippingMethod } from "@/interfaces/interfaces";
+import ShippingMethod from "@/models/ShippingMethod";
+import connectToDB from "@/utils/db";
+
+export const getShippingMethods = async () => {
+  let shippingMethods: IShippingMethod[] = [];
+  try {
+    await connectToDB();
+    shippingMethods = await ShippingMethod.find({}).exec();
+  } catch (error) {
+    console.log((error as Error).message);
+  }
+  console.log("shippingMethods: ", shippingMethods);
+
+  return JSON.parse(JSON.stringify(shippingMethods));
+};
