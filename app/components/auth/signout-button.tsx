d35855955eefe777dcba05cfc8c4b46704b58279
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-29 19:21:11 +0100

    clean up

--- app/components/auth/signout-button.tsx
+++ app/components/auth/signout-button.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { Button } from "@mantine/core";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { signOut } from "next-auth/react";
 
 type Props = {};
 
 const SignoutButton = (props: Props) => {
-  const { data: session } = useSession();
-  //  if (!session) redirect("/loggain");
   return (
     <Button variant="filled" onClick={() => signOut()}>
-      Looga ut
+      Logga ut
     </Button>
   );
 };
