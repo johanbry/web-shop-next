5daab156ca419c081c4921784fdad266ff41406a
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-29 19:19:29 +0100

    redirect if logged in

--- app/(store)/skapakonto/page.tsx
+++ app/(store)/skapakonto/page.tsx
@@ -1,9 +1,14 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import CreateAccountForm from "@/app/components/auth/create-account-form";
 import { Container, Title, Text, Paper } from "@mantine/core";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 type Props = {};
 
 export default async function Page(props: Props) {
+  const session = await getServerSession(authOptions);
+  if (session) return redirect("/minasidor");
   return (
     <Container size="xs" px={0}>
       <Title order={1}>Skapa konto</Title>
