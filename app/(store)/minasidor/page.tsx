94adebbe2919bc6b72b19de233bd719e5fbe977a
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-29 19:19:12 +0100

    add customer info

--- app/(store)/minasidor/page.tsx
+++ app/(store)/minasidor/page.tsx
@@ -1,6 +1,6 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import SignoutButton from "@/app/components/auth/signout-button";
-import { Text, Container, Title, Button } from "@mantine/core";
+import { Text, Container, Title, Button, Group } from "@mantine/core";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -10,14 +10,24 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const session = await getServerSession(authOptions);
-  console.log("sessionrsc", session);
+  const session = (await getServerSession(authOptions)) as unknown as {
+    user: { name: string; id: string };
+  };
+  console.log("session", session);
+
+  const name = session?.user?.name;
+  const userId = session?.user?.id;
 
   if (!session) redirect("/loggain");
   return (
     <Container size="lg" px={0}>
-      <Title>Mina sidor</Title>
-      <SignoutButton />
+      <Group justify="space-between">
+        <Title>Mina sidor</Title>
+        <SignoutButton />
+      </Group>
+      <Text my="lg">
+        Hej {name}! Här kan du se dina beställningar.{userId}
+      </Text>
     </Container>
   );
 }
