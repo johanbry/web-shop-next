015069a6c2ec2db7f23b8c64ab1ccd3f7e3a05ba
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-01-29 19:18:49 +0100

    customer log in page

--- app/(store)/loggain/page.tsx
+++ app/(store)/loggain/page.tsx
@@ -1,16 +1,26 @@
-import CreateAccountForm from "@/app/components/auth/create-account-form";
 import LoginForm from "@/app/components/auth/login-form";
-import { Container, Title, Text, Paper } from "@mantine/core";
+import { Container, Title, Text, Paper, Center, Button } from "@mantine/core";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
 type Props = {};
 
 export default async function Page(props: Props) {
+  const session = await getServerSession(authOptions);
+  if (session) return redirect("/minasidor");
+
   return (
     <Container size="xs" px={0}>
       <Title order={1}>Logga in</Title>
-      <Text my="lg">Logga in.</Text>
       <Paper radius={5} shadow="xs" p="lg" mt="sm" mb="xl">
         <LoginForm />
+        <Center mt="sm">
+          <Text size="sm">
+            <Link href="/skapakonto">Skapa konto</Link>
+          </Text>
+        </Center>
       </Paper>
     </Container>
   );
