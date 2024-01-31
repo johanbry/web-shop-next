c01f263122878ed4b7320393e957f462d6bce61b
Johan Brynielsson <johan.brynielsson@gmail.com>
2024-02-01 00:30:09 +0100

    remove unused

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./global.css";
 
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
-import { Notifications } from "@mantine/notifications";
 import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
 import "@mantine/notifications/styles.css";
-import CartProvider from "@/context/CartContext";
 import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
